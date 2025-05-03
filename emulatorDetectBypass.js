74bb48c5340cbeddf2b98cd68ce03ad06e1e0c10
ktenzweiler <ktenzweiler@gmail.com>
2025-05-03 12:31:58 -0500

    updated deobfuscation

---
+++ emulatorDetectBypass.js
@@ -0,0 +1,17 @@
+
+
+Java.perform(function(){
+
+    // Solution 1
+    var rootUtil = Java.use('com.opthalmologic.salicylize.awfullooking');
+    rootUtil.recalling.implementation = function() {
+
+        var origReturnValue = this.recalling();
+
+        send("Original return value is: " + origReturnValue);
+
+        return false;
+
+    }
+
+});
